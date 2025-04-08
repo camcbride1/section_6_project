1594ff0995ad440d4b7dff926012a52fb53850e9
Colin McBride <11056834@live.mercer.edu>
2025-04-08 14:14:22 -0400

    Test 2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x111111);
+scene.background = new THREE.Color(0x050505);
 
 const camera = new THREE.PerspectiveCamera(
   75, window.innerWidth / window.innerHeight, 0.1, 1000
@@ -19,6 +19,9 @@ scene.add(cube);
 const light = new THREE.PointLight(0xffffff, 1);
 light.position.set(5, 5, 5);
 scene.add(light);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1); // Color and intensity
+scene.add(ambientLight);
 
 camera.position.z = 5;
 
